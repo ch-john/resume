2b4b711f91c9ed3bb512f1b6cd721d3bed0dfc94
ch-john <73250047+ch-john@users.noreply.github.com>
2024-03-15 12:28:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function beersong(beercount) {
+    beercount = 99;
+    while (beercount > 0) {
+        return beercount + " bottles of beer on the wall, " + 99 + " bottles of beer. Take one down and pass it around," + beercount - 1 + " bottles of beer on the wall.";
+        beercount = beercount - 1;
+        
+    }
+
+}
+beersong(99);
